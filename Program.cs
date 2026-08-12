d0ba15a4d882318876ab5b82eafcff64fe0ae8cc
Shradha Sharma <198.shradha@gmail.com>
2026-08-12 10:26:11 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,3 @@
-﻿
-/*using System;
-
-class Program
+﻿class Program
 {
-    static void Main()
-    {
-        TestStudent test = new TestStudent();
-
-        test.Test();
-
-        Console.ReadLine();
-    }
-}*/
-/*using System;
-
-class Program
-{
-    static void Main()
-    {
-        Student s = new Student();
-
-        s.Display();
-
-        Console.ReadLine();
-    }
-}*/
+}
---
+++ ResearchStudent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using StudentLibrary;
+
+class ResearchStudent : Student
+{
+    public void ShowData()
+    {
+        Console.WriteLine("Access Through Inheritance");
+        Console.WriteLine("--------------------------");
+        // public
+        Console.WriteLine("Name       : " + Name);
+        // protected
+        Console.WriteLine("Department : " + Department);
+        // protected internal
+        Console.WriteLine("College    : " + College);
+    }
+
+    static void Main()
+    {
+        ResearchStudent r = new ResearchStudent();
+
+        r.ShowData();
+        Console.WriteLine();
+        Console.WriteLine("Access Through Student Object");
+        Console.WriteLine("-----------------------------");
+
+        Student s = new Student();
+        Console.WriteLine("Name : " + s.Name);
+        Console.ReadLine();
+    }
+}
